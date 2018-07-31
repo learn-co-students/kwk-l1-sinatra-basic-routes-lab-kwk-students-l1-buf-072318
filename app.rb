b984c7193f1e4f081cac19780f527347c9107e71
Dagny-Rebhan <drebhan@buffaloseminary.org>
2018-07-31 19:09:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+  get '/name' do
+    "My name is minkny"
+  end
+  
+  get '/hometown' do
+    "My hometown is Mars"
+  end
+  get '/favorite-song' do
+    "My favorite song is rain"
 end
+end 
