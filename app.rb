433a7e8555025dff5661ee8d0cd5d0dff13c9d22
LizaCotter <ecotter20@allendalecolumbia.org>
2018-07-31 19:04:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Peyton"
+  end 
+  get '/hometown' do
+    "My hometown is Buffalo"
+  end
+  get '/favorite-song' do
+    "My favorite song is You Belong With Me"
+  end
 end
